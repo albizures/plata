55b01e761048559662ad660f8ddf9f54887cb48d
Jose Albizures <albizures3601@gmail.com>
2020-02-29 17:01:48 +0300

    fix(styles): make styles prop allow partial styles

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -3,11 +3,11 @@ export type Children = Child | Child[];
 export type Styles = Partial<CSSStyleDeclaration>;
 
 export type Component<P = {}> = (
-  props: P & { children?: Children }
+	props: P & { children?: Children },
 ) => HTMLElement;
 
-type TargetEvent<E, T> = Omit<E, "target"> & {
-  target: T;
+type TargetEvent<E, T> = Omit<E, 'target'> & {
+	target: T;
 };
 
 export type HEvent<T> = TargetEvent<Event, T>;
@@ -15,31 +15,32 @@ export type HMouseEvent<T> = TargetEvent<MouseEvent, T>;
 export type HChangeEvent<T> = HEvent<T>;
 
 export interface EventHandlers<T> {
-  onChange?: (event: HChangeEvent<T>) => void;
-  onClick?: (event: HEvent<T>) => void;
+	onChange?: (event: HChangeEvent<T>) => void;
+	onClick?: (event: HEvent<T>) => void;
 }
 
 export interface Ref<T = null> {
-  current: T | null;
-  onReady: (handler: () => void) => void;
+	current: T | null;
+	onReady: (handler: () => void) => void;
 }
 
 export type ElementNames = keyof HTMLElementTagNameMap;
-export type Attributes<T> = Partial<Omit<T, "children">> & {
-  children?: Children;
-  ref?: Ref<T>;
+export type Attributes<T> = Partial<Omit<T, 'children' | 'styles'>> & {
+	children?: Children;
+	styles: Styles;
+	ref?: Ref<T>;
 } & EventHandlers<T>;
 
 export type Elements = {
-  [P in ElementNames]: Attributes<HTMLElementTagNameMap[P]>;
+	[P in ElementNames]: Attributes<HTMLElementTagNameMap[P]>;
 };
 
 declare global {
-  namespace JSX {
-    export interface IntrinsicElements extends Elements {}
-    export interface Element extends HTMLElement {}
-    export interface IntrinsicAttributes {
-      key?: any;
-    }
-  }
+	namespace JSX {
+		export interface IntrinsicElements extends Elements {}
+		export interface Element extends HTMLElement {}
+		export interface IntrinsicAttributes {
+			key?: any;
+		}
+	}
 }
