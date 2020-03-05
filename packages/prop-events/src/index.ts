8c28c55a60bd5706277337a008eeff50422ca452
Jose Albizures <albizures3601@gmail.com>
2020-03-06 00:33:17 +0300

    fix(events): fix types

--- packages/prop-events/src/index.ts
+++ packages/prop-events/src/index.ts
@@ -1,28 +1,31 @@
-import {
-	ElementNames,
-	Attributes,
-	EventHandlers,
-} from '@plata/core/dist/types';
+import { ElementNames, Attributes, EventHandlers } from '@plata/core/src/types';
 import * as P from '@plata/core';
 
 const addEventHandler = <E extends ElementNames>(
 	element: HTMLElement,
-	props: EventHandlers<HTMLElementTagNameMap[E]>,
+	props: Attributes<HTMLElementTagNameMap[E]>,
 	name: string,
 	customName: keyof EventHandlers<HTMLElementTagNameMap[E]>,
 ) => {
 	if (props && props[customName]) {
-		//@ts-ignore
-		element.addEventListener(name, props[customName]);
+		element.addEventListener(
+			name,
+			props[customName] as EventListenerOrEventListenerObject,
+		);
 		Reflect.deleteProperty(props, customName);
 	}
 };
 
 const initPlugin = (plata = P) => {
-	plata.plugins.push(<T extends HTMLElement>(el: T, attr: Attributes<T>) => {
-		addEventHandler(el, attr, 'click', 'onClick');
-		addEventHandler(el, attr, 'change', 'onChange');
-	});
+	plata.plugins.push(
+		<E extends ElementNames>(
+			el: HTMLElementTagNameMap[E],
+			attr: Attributes<HTMLElementTagNameMap[E]>,
+		) => {
+			addEventHandler(el, attr, 'click', 'onClick');
+			addEventHandler(el, attr, 'change', 'onChange');
+		},
+	);
 };
 
 initPlugin();
